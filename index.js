6ece570f7aea7e1f9b6854c59aa45c7ddae5165f
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2022-04-10 22:02:36 +0200

    generate random quotes of simpson characters

---
+++ index.js
@@ -0,0 +1,18 @@
+function fetchSimpsonQuotesJSON() {
+  const url = `https://simpsons-quotes-api.herokuapp.com/quotes/`;
+  axios
+    .get(url)
+    .then(function (response) {
+      return response.data;
+    })
+    .then(function (simpsonQuote) {
+      console.log("data decoded from JSON:", simpsonQuote);
+      const simpsonQuoteHtml = `
+        <p><strong>${simpsonQuote[0].character}</strong></p>
+        <img src="${simpsonQuote[0].image}" />
+        <p>${simpsonQuote[0].quote}</p>
+      `;
+      document.querySelector("#simpson-quote").innerHTML = simpsonQuoteHtml;
+    });
+}
+fetchSimpsonQuotesJSON();
